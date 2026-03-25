6177be78a348248e9b3b56e58546cdb32daed95c
Rishi <rishi.soni@nstarxinc.com>
2026-03-25 10:43:35 +0530

    changes

--- 07mypointers/main.go
+++ 07mypointers/main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println("Welcome to a class on pointers")
+
+	// var ptr *int
+	// fmt.Println("Value of pointer is ", ptr)
+
+	myNumber := 23
+
+	var ptr = &myNumber
+
+	fmt.Println("Value of actual pointer is ", ptr)
+	fmt.Println("Value of actual pointer is ", *ptr)
+
+	*ptr = *ptr + 2
+	fmt.Println("New value is: ", myNumber)
 
 }
+
+// &: When to use it: When you need to pass a reference to a variable rather than a copy of its value.
+// *:
+// A. When * is in front of a type (like *int or *User), it’s just a label. It tells Go, "This variable doesn't hold data; it holds a memory address."
+// B. When you put * in front of a variable that is already a pointer, you are dereferencing it. It means, "Follow the map and show me what’s actually inside the house."
---
+++ 08myarray/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to array in golangs")
+
+	var fruitList [4]string
+
+	fruitList[0] = "Apple"
+	fruitList[1] = "Tomato"
+	fruitList[3] = "Peach"
+
+	fmt.Println("Fruit list is: ", fruitList)
+	fmt.Println("Fruit list is: ", len(fruitList))
+	fmt.Println(fruitList[1])
+	fmt.Printf("Type of data: %T \n", fruitList)
+
+	var vegList = [5]string{"potato", "beans", "mushroom"}
+	fmt.Println("Vegy list is: ", len(vegList))
+
+}
